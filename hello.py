2fbadd618c8e2f266c2bc16060e356fb3ab4f7a1
sally nader <SALLY.NADER.AHMED@hotmail.com>
2025-02-15 14:08:55 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,10 @@
-# Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
-
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello, World! This is a flask web app.'
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(debug=True)
