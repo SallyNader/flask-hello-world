9c4c8ee07169fe86cebf775ead802bac5a6bb18b
sally nader <SALLY.NADER.AHMED@hotmail.com>
2025-02-15 16:03:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is a flask web app.'
+    return 'Hello, World! This is a flask web app...'
 
 if __name__ == '__main__':
     app.run(debug=True)
